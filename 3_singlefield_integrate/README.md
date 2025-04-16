0b48319e83bc3af471dcf7e33b009d311d8117cd
Ferraah <ferrariodaniele01@gmail.com>
2025-04-16 11:37:10 +0200

    Removed plotter, adjusted ex3, new ex7 not working.

---
+++ 3_singlefield_integrate/README.md
@@ -0,0 +1,83 @@
+# Monodirectional coupling of a single field with restart file and integration
+
+This examples follows the previous one by enabling the time integration functionality to the coupler. Here our integration operation is `average` instead of `instant`.\
+The parameters are the following: 5 days and 4 exchanges every day, with restarting and save file.
+
+|  | Ocean | Atmosphere|
+|----------|----------|----------|
+|Start date|Jan 01, 2025|Jan 01, 2025 
+| Duration  |  5d       | 5d         |
+|Timestep| 6h | 6h
+| Send/recv frequency          | 4ts          | 4ts         |
+This translates to:
+| freq_op | 4ts| 4ts
+| freq_offset | 0ts | 5ts|
+| (Restart field) freq_op | 1y *| 1y*
+| (Restart field) freq_offset | 0ts | 1ts|
+
+\* arbitrarily large, so to load one time during the run
+
+## Algorithm explaination
+We are exploiting the **time filter** functionality of XIOS. 
+As before, the field is sent by the model each timestep and saved in a buffer. We define the operation to perform on this field reference, `field2D_send`.
+We enable the time integration adding `expr="@this"` to the other field in the coupler * and it means that we are extracting the value of the operation integrating each `freq_op` timesteps.  
+
+*equivalent to`expr="@field2D_send"` because field_ref already points directly to that field.
+
+
+```xml 
+<!-- Accumulate and average on this field --> 
+<field_definition>
+  <field_definition>
+      <field id="field2D_send" grid_ref="grid_2D" operation="average"/>
+  </field_definition>
+</field_definition>
+
+<coupler_out_definition>
+    <coupler_out context="atm::atm" >
+        <field id="field2D_oce_to_atm" field_ref="field2D_send" freq_op="4ts" expr="@this"/>
+        ...
+        ...
+```
+
+## Detail on saving the last field
+As we see in the file definitions:
+```xml
+<!-- Save field on file after 5d (The last send, corresponding to the run duration)-->
+<file id="restart_next" name="restart_next" output_freq="5d" type="one_file" enabled="true" append="false">
+    <field field_ref="field2D_oce_to_atm" operation="instant"/>
+</file>
+```
+We refer to `field2D_oce_to_atm`. `output_freq` attribute for files enable by itself the time integration, and indeed, even if it's hidden, it is performing the op instant after 5 days on our already integrated field and save it to file.
+
+
+# Output
+```
+  ATM: receiving restart field @ts=           1  with value 
+0.000000000000000E+000
+OCN: sending field @ts=           1  with value    1.00000000000000     
+OCN: sending field @ts=           2  with value    2.00000000000000     
+OCN: sending field @ts=           3  with value    3.00000000000000     
+OCN: sending field @ts=           4  with value    4.00000000000000     
+  ATM: receiving field @ts=           5  with value    2.50000000000000     
+OCN: sending field @ts=           5  with value    5.00000000000000     
+OCN: sending field @ts=           6  with value    6.00000000000000     
+OCN: sending field @ts=           7  with value    7.00000000000000     
+OCN: sending field @ts=           8  with value    8.00000000000000     
+  ATM: receiving field @ts=           9  with value    6.50000000000000     
+OCN: sending field @ts=           9  with value    9.00000000000000     
+OCN: sending field @ts=          10  with value    10.0000000000000     
+OCN: sending field @ts=          11  with value    11.0000000000000     
+OCN: sending field @ts=          12  with value    12.0000000000000     
+  ATM: receiving field @ts=          13  with value    10.5000000000000     
+OCN: sending field @ts=          13  with value    13.0000000000000     
+OCN: sending field @ts=          14  with value    14.0000000000000     
+OCN: sending field @ts=          15  with value    15.0000000000000     
+OCN: sending field @ts=          16  with value    16.0000000000000     
+  ATM: receiving field @ts=          17  with value    14.5000000000000     
+OCN: sending field @ts=          17  with value    17.0000000000000     
+OCN: sending field @ts=          18  with value    18.0000000000000     
+OCN: sending field @ts=          19  with value    19.0000000000000     
+OCN: sending field @ts=          20  with value    20.0000000000000 
+```
+
---
+++ 7_multiple_puts/7_multiple_puts.f90
@@ -0,0 +1,188 @@
+program basic_couple
+    use xios
+    implicit none
+    include "mpif.h"
+
+    integer :: ierr, provided
+    integer :: rank, size
+    character(len=255) :: model_id
+
+    type :: toymodel_config
+        type(xios_date) :: start_date, end_date, curr_date
+        type(xios_duration) :: timestep, duration
+        integer :: freq_op_in_ts
+        integer :: ni_glo, nj_glo
+        character(len=255) :: field_type
+    end type toymodel_config
+
+    ! Mpi initialization
+    call MPI_INIT_THREAD(MPI_THREAD_MULTIPLE, provided, ierr)
+    if (provided < MPI_THREAD_MULTIPLE) then
+        print *, "The MPI library does not provide the required level of thread support."
+        call MPI_FINALIZE(ierr)
+        stop
+    end if
+    call MPI_COMM_SIZE(MPI_COMM_WORLD, size, ierr)
+    call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierr)
+
+    ! if (size /= 3) then
+    !     print *, "This program must be run with 3 processes. Currently, there are ", size, " processes."
+    !     call MPI_FINALIZE(ierr)
+    !     stop
+    ! end if 
+    ! -------------------------------
+
+    if(rank==0) then
+        call xios_init_server()
+    else if (rank==1) then
+        model_id = "atm"
+        call run_toymodel()
+    else if (rank==2) then
+        model_id = "ocn"
+        call run_toymodel()
+    else 
+        model_id = "ice"
+        call run_toymodel()
+    end if
+
+    call MPI_FINALIZE(ierr)
+
+contains
+    
+    subroutine run_toymodel()
+    implicit none
+        integer :: local_comm
+        type(xios_context) :: ctx
+        type(toymodel_config) :: config
+        
+        ! Standard XIOS initialization
+        call xios_initialize(model_id, return_comm = local_comm)
+        call xios_context_initialize(model_id, local_comm)
+        call xios_get_handle(model_id, ctx)
+        call xios_set_current_context(ctx)
+
+        if (rank/=3) then
+        ! --------------------------------------------
+
+        ! Loading the configuration of the toy model
+        call load_toymodel_data(config) 
+
+        ! Set the data coming from the model in XIOS
+        call configure_xios_from_model(config)
+
+        ! Run the coupling
+        call run_coupling(config)
+
+        ! --------------------------------------------
+
+        end if
+
+        call xios_context_finalize()
+        call xios_finalize()
+
+
+    end subroutine run_toymodel
+
+    
+    subroutine load_toymodel_data(config)
+        implicit none
+        type(toymodel_config), intent(out) :: config
+        character(len=255) :: tmp = ""
+        type(xios_duration) :: tmp2
+
+        ierr = xios_getvar("toymodel_duration", tmp)
+        print *, "Duration: ", TRIM(tmp)
+        config%duration = xios_duration_convert_from_string(TRIM(tmp))
+        tmp = ""
+
+        ierr = xios_getvar("toymodel_timestep_duration", tmp)
+        print *, "Timestep duration: ", TRIM(tmp)
+        config%timestep = xios_duration_convert_from_string(TRIM(tmp))
+        tmp = ""
+
+        ierr = xios_getvar("toymodel_ni_glo", tmp)
+        read (tmp, *) config%ni_glo
+        tmp = ""
+
+        ierr = xios_getvar("toymodel_nj_glo", tmp)
+        read (tmp, *) config%nj_glo
+        tmp = ""
+
+        ierr = xios_getvar("toymodel_type", tmp)
+        config%field_type = TRIM(tmp)
+        tmp = ""
+        print *, "Field type: ", config%field_type
+
+        ! Getting the frequency of the operation
+        CALL xios_get_field_attr("field2D_oce_to_atm", freq_op=tmp2)
+        CALL xios_duration_convert_to_string(tmp2, tmp)
+        ! Remove the last two characters from the string to retrieve the pure number "(xx)ts"
+        tmp = tmp(1:LEN_TRIM(tmp)-2)
+        ! Convert to integer
+        READ(tmp, *) config%freq_op_in_ts
+        print *, "Frequency of operation: ", config%freq_op_in_ts
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        call xios_get_start_date(config%start_date)
+
+
+    end subroutine load_toymodel_data
+
+    subroutine configure_xios_from_model(config)
+    implicit none
+        type(toymodel_config), intent(in) :: config
+
+        call xios_set_timestep(config%timestep)
+        call xios_set_domain_attr("domain", ni_glo=config%ni_glo, nj_glo=config%nj_glo, type=config%field_type)
+        call xios_close_context_definition()
+
+    end subroutine configure_xios_from_model
+ 
+    subroutine run_coupling(conf)
+    implicit none 
+        type(toymodel_config) :: conf 
+        double precision, pointer:: field_send(:,:), field_recv(:,:)
+        integer :: curr_timestep
+        allocate(field_send(conf%ni_glo, conf%nj_glo))
+        allocate(field_recv(conf%ni_glo, conf%nj_glo))
+
+        conf%end_date = conf%start_date + conf%duration
+        conf%curr_date = conf%start_date
+        curr_timestep = 1
+
+        print *, "Start date: ", conf%start_date
+        print *, "End date: ", conf%end_date
+
+        do while (conf%curr_date < conf%end_date)
+
+            call xios_update_calendar(curr_timestep)
+
+            if (model_id=="ocn") then
+                
+                field_send = curr_timestep
+                print *, "OCN: sending field @ts=", curr_timestep, " with value ", field_send(1,1)
+                call xios_send_field("field2D_send", field_send)
+
+            else if (model_id=="atm") then
+
+                if(curr_timestep==1) then
+                    call xios_recv_field("field2D_restart", field_recv)
+                    print *, "  ATM: receiving restart field @ts=", curr_timestep, " with value ", field_recv(1,1)
+                else if (mod(curr_timestep-1, conf%freq_op_in_ts) == 0) then
+                    call xios_recv_field("field2D_recv", field_recv)
+                    print *, "  ATM: receiving field @ts=", curr_timestep, " with value ", field_recv(1,1)
+                end if
+
+            end if
+
+            conf%curr_date = conf%curr_date + conf%timestep
+            curr_timestep = curr_timestep + 1
+        end do
+
+        if(model_id=="ocn") deallocate(field_send)
+        deallocate(field_recv)
+        
+    end subroutine  run_coupling 
+
+
+end program
